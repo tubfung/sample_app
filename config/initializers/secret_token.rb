16b4aa6df2c915c20915b5d2e7d7280f2aa3acf4
tubfung <bob@nuvana.org>
2012-03-29 13:13:07 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '4ce304b4c5dd7a8a84e8a939947769ab8c5b26bf85f27e76840561250099d6be8bf70520adb7e3c768271a5f50909d35b7b8b1d7ea50332f104b9f83398fc8c0'
